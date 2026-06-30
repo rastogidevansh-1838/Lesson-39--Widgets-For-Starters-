daaba17d4101368d04f83bc08f6144d2c2548ad1
rastogidevansh-1838 <rastogi.kopal@gmail.com>
2026-06-30 09:51:09 +0530

    fg

---
+++ hw.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import math
+window = Tk()
+window.title("Product of numberssssss")
+window.geometry("500x500")
+lbl = Label(text = "the product of 7 and 8 is....", fg = "black", bg = "#729357", height = 1, width = 300)
+calculation = ("7x8")
+text = "the ans is" +calculation+ "\n"
+window.mainloop()
